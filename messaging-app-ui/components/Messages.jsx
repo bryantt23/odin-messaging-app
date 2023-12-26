74bfab3e841b82124e270caace38ab170eb461dc
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-26 02:32:33 -0800

    show messages

---
+++ messaging-app-ui/components/Messages.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Messages() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/messages')
+            .then(response => response.json())
+            .then(data => setMessages(data))
+            .catch(error => console.error('Error fetching messages:', error));
+    }, []);
+
+    return (
+        <div className="messages-container">
+            <h2 className="messages-title">All Messages</h2>
+            {messages.length > 0 ? (
+                <ul className="message-list">
+                    {messages.map(message => (
+                        <li key={message._id} className="message-item">
+                            <h3 className="message-title">{message.userId}</h3>
+                            <p className="message-body">{message.content}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No messages available.</p>
+            )}
+        </div>
+    );
+}
+
+export default Messages;
