6a61374f3814d3aea81316f5645c1a8ea5a34210
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-26 03:40:47 -0800

    can add anonymous message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Message = require('./models/Message');
+const User = require('./models/User');
 
 require('dotenv').config();
 // MongoDB Connection
@@ -14,6 +15,8 @@ mongoose
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
@@ -41,6 +44,27 @@ app.get('/messages', async (req, res) => {
   } catch (error) {
     console.error('Error fetching messages:', error);
     res.status(500).send('Error fetching messages');
+  }
+});
+
+app.post('/messages', async (req, res) => {
+  try {
+    const anonymousUser = await User.findOne({ name: 'anonymous' });
+
+    if (!anonymousUser) {
+      return res.status(404).send('Anonymous user not found');
+    }
+    // Create a new message with the anonymous user as the author
+    const newMessage = new Message({
+      userId: anonymousUser._id,
+      content: req.body.content // Assuming the message content is sent in the request body
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error('Error posting message:', error);
+    res.status(500).send('Error posting message');
   }
 });
 
--- messaging-app-ui/components/Messages.jsx
+++ messaging-app-ui/components/Messages.jsx
@@ -1,15 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 function Messages() {
     const [messages, setMessages] = useState([]);
+    const [text, setText] = useState("");
 
     useEffect(() => {
-        fetch('http://localhost:3000/messages')
-            .then(response => response.json())
-            .then(data => setMessages(data))
-            .catch(error => console.error('Error fetching messages:', error));
+        async function fetchData() {
+            const response = await getMessages()
+            setMessages(response)
+        }
+        fetchData();
     }, []);
+
+    const getMessages = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/messages')
+            return response.json()
+
+        } catch (error) {
+            console.error('Error fetching messages:', error)
+            return error
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:3000/messages', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ content: text })
+            })
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const messages = await getMessages()
+            setText("")
+            setMessages(messages)
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    }
+
 
     return (
         <div className="messages-container">
@@ -26,6 +61,10 @@ function Messages() {
             ) : (
                 <p>No messages available.</p>
             )}
+            <form>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)}></input>
+                <input type="submit" onClick={handleSubmit}></input>
+            </form>
         </div>
     );
 }
