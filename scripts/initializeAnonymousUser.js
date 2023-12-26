751de28b9750ddba2cc6fd2a12c173db971dfef4
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-26 03:08:57 -0800

    cleanup

--- scripts/initializeAnonymousUser.js
+++ scripts/initializeAnonymousUser.js
@@ -1,40 +1,37 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/User'); // Assuming you have a User model defined as previously discussed
+const User = require('../models/User'); // Update the path as per your project structure
 require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
+  .connect(process.env.MONGODB_URI)
+  .then(async () => {
     console.log('Connected to MongoDB');
-    createAnonymousUser();
+    await createAnonymousUser();
   })
   .catch(err => {
     console.error('Database connection error', err);
-  })
-  .finally(() => {
-    mongoose.disconnect();
   });
 
 async function createAnonymousUser() {
   try {
-    // Check if anonymous user already exists
     let user = await User.findOne({ name: 'anonymous' });
-
     if (!user) {
-      // If not, create a new anonymous user
       const hashedPassword = await bcrypt.hash('password', 10);
       user = await User.create({ name: 'anonymous', password: hashedPassword });
       console.log('Anonymous user created');
     } else {
       console.log('Anonymous user already exists');
     }
-
-    return user;
   } catch (error) {
     console.error('Error creating anonymous user:', error);
   }
 }
+
+// Optional: Handle graceful shutdown
+process.on('SIGINT', () => {
+  mongoose.disconnect().then(() => {
+    console.log('Disconnected from MongoDB');
+    process.exit(0);
+  });
+});
